a2c42c40cd777a4b96a33827aaa58c41577b312f
Nisha Kumar-Mayernik <nisha.kumar-mayernik@intel.com>
2013-09-25 13:27:03 -0700

    PEP8 clean up

--- note.py
+++ note.py
@@ -1,81 +1,85 @@
-'''
-Class: Note
-Description: This contains all the information about the note
-The note has the following attributes:
-- title: the note's title
-- category: what category the note is under
-- source: where the note came from
-- tags: search keywords
-- root: root directory set by the main module
-- store: file where the note contents are stored
-'''
-
 import os
 import hashlib as h
 from datetime import datetime as d
 
+
 class Note:
-  def __init__(self,title,root):
-    self._set_title("title")
-    self.root = root
-    self._set_category("")
-    self._set_source("")
-    self.tags = set()
-    self.store = h.sha1(title+
-                        d.now().strftime('%Y%m%d%H%M%S')).hexdigest()
-    
-  def set_title(self,title):
-    self.title = title
 
-  def set_category(self,category):
-    self.category = category
+    """
+    Class: Note
+    Description: This contains all the information about the note
 
-  def set_source(self,source):
-    self.source = source
+    Attributes:
+    - title: the note's title
+    - category: what category the note is under
+    - source: where the note came from
+    - tags: search keywords
+    - dir: the directory where the note is filed
+    - file: file where the note contents are filed
+    """
 
-  def get_title(self):
-    return self.title
+    def __init__(self, title, dir):
+        self._set_title("title")
+        self.dir = dir
+        self._set_category("")
+        self._set_source("")
+        self.tags = set()
+        # the file name is the title plus the hex digest
+        self.file = h.sha1(title +
+                           d.now().
+                           strftime('%Y%m%d%H%M%S')).hexdigest()
 
-  def get_category(self):
-    return self.category
+    def set_title(self, title):
+        self.title = title
 
-  def get_source(self):
-    return self.source
+    def set_category(self, category):
+        self.category = category
 
-  def get_store(self):
-    return self.store 
+    def set_source(self, source):
+        self.source = source
 
-  _set_title = set_title
-  _set_category = set_category
-  _set_source = set_source 
+    def get_title(self):
+        return self.title
 
-  def add_tag(self,tag):
-    self.tags.append(tag)
+    def get_category(self):
+        return self.category
 
-  def remove_tag(self,tag):
-    success = false
-    if tag in self.tags:
-      self.tags.remove(tag)
-      success = true
-    return success
+    def get_source(self):
+        return self.source
 
-  def edit_tag(self,old_tag,new_tag):
-    success = false
-    if old_tag in self.tags and new_tag not in self.tags:
-      index = self.tags.index(old_tag)
-      self.tags.insert(index,new_tag)
-      success = true
-    return success
+    def get_file(self):
+        return self.file
 
-  def write_data(self,note):
-    path = os.path.join(self.root,self.store)
-    data = open(path,'w')
-    data.write(note)
-    data.close
+    _set_title = set_title
+    _set_category = set_category
+    _set_source = set_source
 
-  def read_data(self):
-    path = os.path.join(self.root,self.store)
-    data = open(path,'r')
-    content = data.read()
-    return content
+    def add_tag(self, tag):
+        self.tags.append(tag)
 
+    def remove_tag(self, tag):
+        success = false
+        if tag in self.tags:
+            self.tags.remove(tag)
+            success = true
+        return success
+
+    def replace_tag(self, old_tag, new_tag):
+        success = false
+        if old_tag in self.tags and new_tag not in self.tags:
+            index = self.tags.index(old_tag)
+            self.tags.insert(index, new_tag)
+            success = true
+        return success
+
+    def write_data(self, content):
+        path = os.path.join(self.dir, self.file)
+        data = open(path, 'w')
+        data.write(content)
+        data.close
+
+    def read_data(self):
+        path = os.path.join(self.dir, self.file)
+        data = open(path, 'r')
+        content = data.read()
+        return content
